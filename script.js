477ce4815886e6478da902dec843b033b20e63a2
dimarique <pochtagreen@gmail.com>
2024-07-09 16:35:38 +0200

    done positioning and styling the search bar

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ import { words } from './data.js';
 const checkButton = document.querySelector('.check_btn');
 const nextButton = document.querySelector('.next_btn');
 const hiddenWord = document.querySelectorAll('.hidden_word');
+const searchButton = document.querySelector('.searchToggler');
+const searchBar = document.querySelector('.searchBar');
 const cardNumber = randomCard();
+
+function searchBarToggle() {
+	searchBar.classList.toggle('hidden');
+	searchBar.value = '';
+}
 
 function randomCard() {
 	return Math.floor(Math.random() * words.length);
@@ -15,12 +22,14 @@ function showHidden() {
 	})
 }
 
-checkButton.addEventListener('click', showHidden);
-
 nextButton.addEventListener('click', function() {
 	location.reload();
 	return false;
 });
+
+
+checkButton.addEventListener('click', showHidden);
+searchButton.addEventListener('click', searchBarToggle);
 
 document.querySelector('.first_form').innerText = words[cardNumber][0];
 document.querySelector('.second_form').innerText = words[cardNumber][1];
