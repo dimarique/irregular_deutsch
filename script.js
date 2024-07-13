a5e44d766cef1e2aade34b4e8b23f233f16a77db
dimarique <pochtagreen@gmail.com>
2024-07-13 21:24:15 +0200

    work with filter functionality

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 import { words } from './data.js';
 
+//in debugging purpose
+window.words = words
+
+
 const checkButton = document.querySelector('.check_btn');
 const nextButton = document.querySelector('.next_btn');
 const hiddenWord = document.querySelectorAll('.hidden_word');
 const searchButton = document.querySelector('.searchToggler');
 const searchBar = document.querySelector('.searchBar');
 const cardNumber = randomCard();
+
+const selectors = ['.first_form', '.second_form', '.third_form', '.fourth_form', '.translate'];
 
 //Toggle visibility of the search bar
 function searchBarToggle() {
@@ -33,7 +39,13 @@ nextButton.addEventListener('click', function() {
 checkButton.addEventListener('click', showHidden);
 searchButton.addEventListener('click', searchBarToggle);
 
-document.querySelector('.first_form').innerText = words[cardNumber][0];
+//function updateFormFields(cardNumber) {
+//selectors.forEach((selector, index) => {
+//document.querySelector(selector).innerText = words[cardNumber][index];
+//});
+//}
+//updateFormFields()
+document.querySelector(selectors[0]).innerText = words[cardNumber][0];
 document.querySelector('.second_form').innerText = words[cardNumber][1];
 document.querySelector('.third_form').innerText = words[cardNumber][2];
 document.querySelector('.fourth_form').innerText = words[cardNumber][3];
