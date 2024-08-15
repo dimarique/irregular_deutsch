1153fe2b0147555ab8e3ff989d21d096544423cc
dimarique <pochtagreen@gmail.com>
2024-08-15 09:22:27 +0200

    optimisation

--- script.js
+++ script.js
@@ -39,14 +39,6 @@ nextButton.addEventListener('click', function() {
 checkButton.addEventListener('click', showHidden);
 searchButton.addEventListener('click', searchBarToggle);
 
-//function updateFormFields(cardNumber) {
-//selectors.forEach((selector, index) => {
-//document.querySelector(selector).innerText = words[cardNumber][index];
-//});
-//}
-//updateFormFields()
-document.querySelector(selectors[0]).innerText = words[cardNumber][0];
-document.querySelector(selectors[1]).innerText = words[cardNumber][1];
-document.querySelector(selectors[2]).innerText = words[cardNumber][2];
-document.querySelector(selectors[3]).innerText = words[cardNumber][3];
-document.querySelector(selectors[4]).innerText = words[cardNumber][4];
+selectors.forEach((selector, index) => {
+	document.querySelector(selector).innerText = words[cardNumber][index];
+});
