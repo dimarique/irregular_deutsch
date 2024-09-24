c9f031052496d15bb67d8c788a8d3a24f62d9ac5
dimarique <pochtagreen@gmail.com>
2024-09-24 11:28:14 +0200

    hidden word functionality add

--- verbs_prep.js
+++ verbs_prep.js
@@ -4,6 +4,7 @@ import { verbsMitPrep } from './data.js'
 //in debugging purpose
 window.commonPrepositions = commonPrepositions
 window.verbsMitPrep = verbsMitPrep
+window.randomCard = randomCard
 
 const elements = {
 	nextButton: document.querySelector('.next_btn'),
@@ -12,8 +13,31 @@ const elements = {
 	beispilText: document.querySelector('.beispil'),
 	verbMitPrep: document.querySelector('.verbMitPrep'),
 	explanation: document.querySelector('.explanation'),
-	cardNumber: randomCard()
+	cardNumber: randomCard(),
+	buttons: document.querySelectorAll('.btn'),
 }
+elements.specialWordButtonIndex = getRandomIndex(elements.buttons);
+elements.prepToHide = verbsMitPrep[elements.cardNumber][0][1];
+const wordsWithoutSpecial = commonPrepositions.filter(word => word !== elements.prepToHide);
+elements.randomWords = getRandomWords(wordsWithoutSpecial, 4)
+
+function getRandomIndex(array) {
+	return Math.floor(Math.random() * array.length);
+}
+
+function getRandomWords(arr, count) {
+	const shuffled = [...arr].sort(() => 0.5 - Math.random());
+	return shuffled.slice(0, count);
+}
+
+function hidePrepInText(text) {
+	const hiddenWord = `<span class="hidden_word">${elements.prepToHide}</span>`;
+	return text.replace(elements.prepToHide, hiddenWord);
+}
+
+elements.beispilText.innerHTML = hidePrepInText(verbsMitPrep[elements.cardNumber][1].join(' '));
+elements.verbMitPrep.innerHTML = hidePrepInText(verbsMitPrep[elements.cardNumber][0].join(' '));
+
 
 
 //Toggle visibility of the search bar
@@ -22,6 +46,20 @@ function searchBarToggle() {
 	elements.searchBar.value = '';
 }
 
+elements.buttons.forEach((button, index) => {
+	if (index === elements.specialWordButtonIndex) {
+		button.textContent = elements.prepToHide;
+	} else {
+		button.textContent = elements.randomWords.pop();
+	}
+	button.addEventListener('click', () => {
+		if (button.textContent === elements.prepToHide) {
+			document.querySelectorAll('.hidden_word').forEach(span => {
+				span.classList.remove('hidden_word');
+			});
+		}
+	});
+});
 
 elements.nextButton.addEventListener('click', function() {
 	location.reload();
@@ -31,28 +69,23 @@ elements.nextButton.addEventListener('click', function() {
 
 elements.searchButton.addEventListener('click', searchBarToggle);
 
-/*
- *selectors.forEach((selector, index) => {
- *    document.querySelector(selector).innerText = words[cardNumber][index];
- *});
- */
-
-
 function randomCard() {
 	return (Math.floor(Math.random() * verbsMitPrep.length));
 
 }
 
 
-elements.beispilText.innerText = verbsMitPrep[elements.cardNumber][1].join(' ');
-elements.verbMitPrep.innerText = verbsMitPrep[elements.cardNumber][0].join(' ');
-elements.explanation.innerText = verbsMitPrep[elements.cardNumber][2];
 /*
-/*
- *Shows hidden words
- *function showHidden() {
- *hiddenWord.forEach(function(word) {
- *word.classList.remove('hidden_word');
- *})
- *}
+ *elements.beispilText.innerText = verbsMitPrep[elements.cardNumber][1].join(' ');
+ *elements.verbMitPrep.innerText = verbsMitPrep[elements.cardNumber][0].join(' ');
  */
+elements.explanation.innerText = verbsMitPrep[elements.cardNumber][2];
+
+
+
+console.log(elements.prepToHide, elements.verbMitPrep.innerHTML);
+
+
+
+
+
